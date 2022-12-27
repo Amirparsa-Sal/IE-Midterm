ebe751ae9230409939062bb0ae44be5027755f22
Amirparsa-Sal <Amirparsa.s751@gmail.com>
2022-12-27 20:41:31 +0330

    complete doc for js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let error_rgba = [129, 7, 7, 0.77]
 let local_storage_rgba = [255, 215, 7, 0.77]
 
 function clearStatus(){
+    /**
+     *   Cleares the status box. 
+     *   The status box is used to display errors and other messages.
+    */
     var status_container = document.getElementById("status-container");
     var error_box = document.getElementById("status-box");
     status_container.style.display = "None";
@@ -9,6 +13,11 @@ function clearStatus(){
 }
 
 function displayStatus(text, rgba){
+    /** 
+     * Displays a message in the status box.
+     * @param {string} text - The text to display.
+     * @param {list} rgba - The rgba values to use for the background color.
+    */
     var status_container = document.getElementById("status-container");
     var error_box = document.getElementById("status-box");
     error_box.innerHTML = text;
@@ -19,6 +28,10 @@ function displayStatus(text, rgba){
 
 function fillInfo(data)
 {
+    /**
+     * Fills the info box with the data from the data dict.
+     * @param {dict} data - The data dict.
+    */
     // get divs
     var blogDiv = document.getElementById("blog-row");
     var locDiv = document.getElementById("loc-row");
@@ -77,6 +90,12 @@ function fillInfo(data)
 }   
 
 async function callAPI(url){
+    /**
+     * Calls the API at the given url.
+     * @param {string} url - The url to call.
+     * @returns {dict} - The data returned by the API.
+     * @throws {Error} - If the API returns an error.
+    */
     let response = await fetch(url);
     // check for errors
     if (!response.ok){
@@ -91,8 +110,7 @@ async function callAPI(url){
 
 // get search button
 var searchButton = document.getElementById("search-button");
-console.log(searchButton);
-// add event listener
+// add a event listener to call APIs and fill data
 searchButton.addEventListener("click", function() {
     // clear status
     clearStatus();
@@ -101,15 +119,14 @@ searchButton.addEventListener("click", function() {
     input = searchInput.value;
     // lower case the input
     input = input.toLowerCase();
-    // check if the data is in local storage
+    // check if the data is in local storage and load it
     if (localStorage.getItem(input) != null){
         var data = JSON.parse(window.localStorage.getItem(input));
-        console.log(data)
         fillInfo(data);
         displayStatus("Loaded from local storage!", local_storage_rgba);
         return;
     }
-    // create data dict to store info
+    // create data dict to store info if the data is not in local storage
     var data = {};
     // Send GET request to get info
     callAPI("https://api.github.com/users/" + input)
@@ -169,12 +186,16 @@ searchButton.addEventListener("click", function() {
         }  
         return data;
     })
+    // update the local storage
     .then((data) => window.localStorage.setItem(input, JSON.stringify(data)))
+    // handle errors and display status
     .catch((error) => {
+        // handle network errors
         if (error instanceof TypeError){
             displayStatus("Network error: " + error.message, error_rgba);
             return;
         }
+        // handle other errors
         displayStatus(error.message, error_rgba);
     });
 });
